132cab15911d1ca4ff0e182e14970ba2302ccd4b
Reece Bradley <naruto73@hotmail.co.uk>
2023-02-22 17:14:51 +0000

    finish basic hangman game

--- hangman.rb
+++ hangman.rb
@@ -1,9 +1,16 @@
 class Hangman
   
-  attr_accessor :guess
+  attr_accessor :turn
+  attr_accessor :hidden_word
+  attr_accessor :chosen_word
+  attr_accessor :wrong_guesses
 
   def initialize
-    @guess = 1
+    @turn = 0
+    @chosen_word = random_word_generator
+    @hidden_word = ""
+    @chosen_word.length.times { @hidden_word += "_" }
+    @wrong_guesses = ""
   end
   
   def rules
@@ -12,9 +19,9 @@ class Hangman
     In this game you have 10 turns to guess a word. A correct guess will give 
     you an extra turn, but if you have ten incorrect guesses, it's Game Over. 
     The word will be chosen randomly and will be between 5 and 12 letters long. 
-    Each turn you will choose a letter or guess the word. If the word contains 
-    your chosen letter, that letter will replace the blank space where it is 
-    located. You will win when you either fill in the blanks or guess the word.
+    Each turn you will choose a letter. If the word contains your chosen letter,
+    that letter will replace the blank space where it is located. You will win 
+    when you either fill in the blanks or guess the word.
     Good Luck.\n\n
     TEXT
   end
@@ -23,15 +30,41 @@ class Hangman
     rules
     print "Choose 1 to start the game: "
     input = gets.chomp.to_i
-    player
+    game
   end
 
-  def player
-    chosen_word = random_word
-    puts chosen_word
-  end
+  def game
+    
+    loop do
+      if @hidden_word == @chosen_word
+        puts "Congratulations, You Won!"
+        puts "The hidden word was #{@hidden_word}."
+        break
+      elsif @turn == 10
+        puts "Sorry, that was your final turn. You Lost!"
+        puts "The hidden word was #{@chosen_word}."
+        break
+      end
 
-  def random_word
+      puts "Incorrect Guesses: " + @wrong_guesses + "\n\n"
+      puts @hidden_word
+      print "Choose a letter: "
+      input = gets.chomp.downcase
+      
+      if @hidden_word != @chosen_word && @chosen_word.include?(input)
+        @chosen_word.split('').each_with_index do |char, idx|
+          @hidden_word[idx] = input if char == input
+        end
+        puts "\nCorrect, that letter is in the word.\n\n"
+      else
+        puts "\nThat letter is not in the word.\n\n"
+        @wrong_guesses += input
+        @turn += 1
+      end
+    end
+  end
+ 
+  def random_word_generator
     words = []
     word_list = File.read('google_10000_english_no_swears.txt')
 
